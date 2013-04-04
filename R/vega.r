3889b8d5b21c9f295bd5985e2c8e7839df716ab8
hadley <h.wickham@gmail.com>
2013-04-04 15:25:29 -0500

    Some more docs to vega

--- R/vega.r
+++ R/vega.r
@@ -1,5 +1,24 @@
+#' Render a ggplot2 graphic with vega.
+#'
+#' @section Use:
+#' This function creates all the files necesary to render a vega visualisation:
+#' but currently you're responsible for running a server to send them to
+#' the browser.  You can't run directly from disk (with a \code{file://})
+#' url, because browser security settings make it impossible to load data
+#' from relative urls.
+#'
+#' @export
+#' @param plot ggplot2 graphic
+#' @param base_path path in which to save all visualisation files.  Must
+#'   already exist.
+#' @param name name of visualisation. Used to name html and js spec files
+#' @param width width of visualisation in pixels
+#' @param height height of visualisation in pixels
+#' @param padding a numeric vector of length 4 giving the padding on the
+#'  top, right, bottom and left sides respectively.
 #' @importFrom whisker whisker.render
-vega <- function(plot, base_path = ".", name = "test", width = 600, height = 400, padding = c(10, 10, 10, 10)) {
+vega <- function(plot, base_path = ".", name = "test", width = 600,
+                 height = 400, padding = c(10, 10, 10, 10)) {
   stopifnot(is.numeric(padding), length(padding) == 4)
   stopifnot(is.character(base_path), length(base_path) == 1,
     file.exists(base_path), is.dir(base_path))
@@ -18,7 +37,7 @@ vega <- function(plot, base_path = ".", name = "test", width = 600, height = 400
   # Render html template
   tmpl <- readLines(system.file("templates", "render.html", package = "gg2v"))
   out <- whisker.render(tmpl, list(
-    title = "My vega vis",
+    title = paste0("gg2v: ", name),
     spec_path = basename(spec_path)))
   writeLines(out, html_path)
 
